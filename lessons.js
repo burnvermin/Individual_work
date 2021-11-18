2db272d4177b22aaf8b983334bd597c5b2ca4ff5
Oluwagbemiga David <39445905+burnvermin@users.noreply.github.com>
2021-11-18 07:34:56 +0100

    Created the array
    File Created

---
+++ lessons.js
@@ -0,0 +1,92 @@
+let lessons = [
+    {
+        "id" : 1,
+        "subject": "Mathematics",
+        "location": "London",
+        "price": 250,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "image": "\\images//Maths.png"
+    },
+    {
+        "id" : 2,
+        "subject": "User Experience",
+        "location": "Alabama",
+        "price": 550,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "image": "\\images//UX.jpeg"
+  },
+  {
+        "id" : 3,
+        "subject": "Web Development",
+        "location": "London",
+        "price": 300,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "image": "\\images//WD.png"
+    },
+    {
+        "id" : 4,
+        "subject": "English",
+        "location": "London",
+        "price": 500,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "image": "\\images//ENG.jpg"
+    },
+    {
+        "id" : 5,
+        "subject": "Python",
+        "location": "London",
+        "price": 199,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "image": "\\images//Py.jpg"
+    },
+    {
+        "id" : 6,
+        "subject": "JavaEE",
+        "location": "London",
+        "price": 499,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "image": "\\images//EE.png"
+    },
+    {
+        "id" : 7,
+        "subject": "Religious Studies",
+        "location": "London",
+        "price": 700,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "image": "\\images//RS.jpg"
+    },
+    {
+        "id" : 8,
+        "subject": "Hunting",
+        "location": "London",
+        "price": 69,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "image": "\\images//Hunt.jpg"
+    },
+    {
+        "id" : 9,
+        "subject": "Music",
+        "location": "London",
+        "price": 299,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "image": "\\images//MS.png"
+    },
+    {
+        "id" : 10,
+        "subject": "Mafia",
+        "location": "Mexico",
+        "price": 10_000,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "image": "\\images//maf.jpg"
+    }
+]
